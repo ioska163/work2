35966edb86fd49959ecf30e755b2cdc8ac8dc632
ioska163 <ershov.max1988@mail.ru>
2024-06-05 19:09:26 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+sum_final_homeworks = 12
+sum_used_hours = 1.5
+name_of_course = ('Python')
+time_spent_on_one_homework = sum_used_hours / sum_final_homeworks
+print('Курс:' + name_of_course + ',' + 'всего задач:' + str(sum_final_homeworks) + ',' + 'затрачено часов:' + str(sum_used_hours) + ',' 'среднее время выполнения: ' + str(time_spent_on_one_homework))
